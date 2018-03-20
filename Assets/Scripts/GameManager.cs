00636551b44efc8d6d479cb8e82335a453e38fca
Nicolas Triebe <n.triebe@gmx.de>
2018-03-20 21:30:39 +0100

    Added Main Scene, Player and GameManager Scripts
    Added Prefabs in and aassociated folder

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameManager : NetworkBehaviour
+{
+
+    float gameTime = 180;
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+        gameTime -= Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+    //Represents a connected Player
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    /// <summary>
+    /// Called by the GameManager when a new Round is started. Spawns the Players Tank 
+    /// on the basis of the TankPrefab.
+    /// </summary>
+    public void SpawnTank()
+    {
+
+    }
+}
